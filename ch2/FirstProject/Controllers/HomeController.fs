0babb7c0ecec0719435513d8b353a76c0ad0668f
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2023-08-30 02:46:27 -0300

    chapter 2

---
+++ ch2/FirstProject/Controllers/HomeController.fs
@@ -0,0 +1,13 @@
+﻿namespace FirstProject.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open System
+
+type HomeController () =
+    inherit Controller()
+
+    member this.Index () =
+        let hour = DateTime.Now.Hour
+        let viewModel = if hour < 12 then "Good Morning" else "Good Afternoon"
+        this.View("MyView", viewModel)
+
